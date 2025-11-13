8454141bc88b8dfc5317175233d3a46d9b645fde
ElTrompa <anroiz@alumnatflorida.es>
2025-11-13 22:09:25 +0100

    estructura

---
+++ supabase-todos/src/hooks/useTodos.js
@@ -0,0 +1,9 @@
+// TODO: Importar useState, useEffect, useCallback
+// TODO: Importar funciones de todosApi.js
+
+export function useTodos() {
+  // TODO: Crear estados: todos, loading, error
+  // TODO: Crear funciones load(), create(), toggle(), remove()
+  // TODO: Cargar datos en useEffect
+  // TODO: Retornar { todos, loading, error, create, toggle, remove }
+}
